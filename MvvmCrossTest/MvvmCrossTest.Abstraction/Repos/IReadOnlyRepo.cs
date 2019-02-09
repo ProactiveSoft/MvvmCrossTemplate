806a0764ffe47da22f2486258f50ed9775ef0e2f
Anil Kumar <anil.apex@outlook.com>
2019-02-09 11:29:39 +0530

    Abstracted repos.

---
+++ MvvmCrossTest/MvvmCrossTest.Abstraction/Repos/IReadOnlyRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using MvvmCrossTest.Abstraction.Models;
+
+namespace MvvmCrossTest.Abstraction.Repos
+{
+    public interface IReadOnlyRepo<out TModel, TId>
+        where TModel : ModelBase<TId>
+        where TId : struct, IComparable<TId>, IEquatable<TId>
+    {
+        TModel ReadAsync(TId id);
+        IQueryable<TModel> ReadAsync();
+        IQueryable<TModel> ReadAsync(Predicate<TId> ids);
+    }
+}
---
+++ MvvmCrossTest/MvvmCrossTest.Abstraction/Repos/IWriteOnlyRepo.cs
@@ -0,0 +1,30 @@
+﻿using MvvmCrossTest.Abstraction.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvvmCrossTest.Abstraction.Repos
+{
+    public interface IWriteOnlyRepo<in TModel, TId>
+        where TModel : ModelBase<TId>
+        where TId : struct, IComparable<TId>, IEquatable<TId>
+    {
+        Task<TId> AddAsync(TModel item);
+        Task<IQueryable<TId>> AddAsync(IEnumerable<TModel> items);
+
+
+
+        Task UpdateAsync(TId id);
+        Task UpdateAsync(IEnumerable<TId> ids);
+        Task UpdateAsync(TModel item);
+        Task UpdateAsync(IEnumerable<TModel> items);
+
+
+
+        Task DeleteAsync(TId id);
+        Task DeleteAsync(IEnumerable<TId> ids);
+        Task DeleteAsync(TModel item);
+        Task DeleteAsync(IEnumerable<TModel> items);
+    }
+}
