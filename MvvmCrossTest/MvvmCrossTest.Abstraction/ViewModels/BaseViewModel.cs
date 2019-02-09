856445af23083a33e4478cfaea662e157650c895
Anil Kumar <anil.apex@outlook.com>
2019-02-09 10:25:41 +0530

    Abstracted Models & ViewModels.

--- MvvmCrossTest/MvvmCrossTest.Abstraction/ViewModels/BaseViewModel.cs
+++ MvvmCrossTest/MvvmCrossTest.Abstraction/ViewModels/BaseViewModel.cs
@@ -1,12 +1,101 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using MvvmCross.Navigation;
+﻿using MvvmCross.Navigation;
 using MvvmCross.ViewModels;
 using MvvmCrossTest.Abstraction.Annotations;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace MvvmCrossTest.Abstraction.ViewModels
 {
     public abstract class BaseViewModel : MvxViewModel, INotifyPropertyChanged
+    {
+        protected BaseViewModel(IMvxNavigationService navigationService)
+        {
+            NavigationService = navigationService;
+        }
+
+
+        public string Title
+        {
+            get => _title;
+            protected set
+            {
+                if (value == _title) return;
+                _title = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        #region INPC
+
+        public new event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+
+        #region Fields
+
+        private string _title;
+
+
+        protected readonly IMvxNavigationService NavigationService;
+
+        #endregion
+    }
+
+
+
+    public abstract class BaseViewModel<TParameter> : MvxViewModel<TParameter>, INotifyPropertyChanged
+    {
+        protected BaseViewModel(IMvxNavigationService navigationService)
+        {
+            NavigationService = navigationService;
+        }
+
+
+        public string Title
+        {
+            get => _title;
+            protected set
+            {
+                if (value == _title) return;
+                _title = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        #region INPC
+
+        public new event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+
+        #region Fields
+
+        private string _title;
+
+
+        protected readonly IMvxNavigationService NavigationService;
+
+        #endregion
+    }
+
+
+    public abstract class BaseViewModel<TParameter, TResult> : MvxViewModel<TParameter, TResult>, INotifyPropertyChanged
     {
         protected BaseViewModel(IMvxNavigationService navigationService)
         {
