c77c53f0b963b4cd0984e95f91f752e284d4e404
Anil Kumar <anil.apex@outlook.com>
2020-02-26 12:39:14 +0530

    Created project to edit template.
    Related Work Items: #584

---
+++ MvvmCross.Template/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MvvmCross.Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Edit template.");
+        }
+    }
+}
