5abb96f678f8be78970c22ebc8f7dc682a9818b5
Anil Kumar <anil.apex@outlook.com>
2020-03-14 07:58:10 +0530

    iOS: Add assets to .vstemplate.
    Related Work Items: #584

--- MvvmCross.Template/FixIosProject.cs
+++ MvvmCross.Template/FixIosProject.cs
@@ -29,10 +29,32 @@ namespace MvvmCross.Template
                 "      <ProjectItem ReplaceParameters=\"false\" TargetFileName=\"Info.plist\"",
                 "      <ProjectItem ReplaceParameters=\"true\" TargetFileName=\"Info.plist\"");
             WriteLine($"Corrected to ReplaceParameters=\"true\" TargetFileName=\"Info.plist\")\n");
+
+
+            WriteLine("\nIncluding assets in .vstemplate file:");
+            string assets =
+                @"      <Folder Name=""Assets.xcassets\AppIcon.appiconset"" TargetFolderName=""Assets.xcassets\AppIcon.appiconset"">
+        <ProjectItem TargetFileName=""Contents.json"" ReplaceParameters=""true"">Contents.json</ProjectItem>
+        <ProjectItem TargetFileName=""Icon20.png"" ReplaceParameters=""false"">Icon20.png</ProjectItem>
+        <ProjectItem TargetFileName=""Icon29.png"" ReplaceParameters=""false"">Icon29.png</ProjectItem>
+        <ProjectItem TargetFileName=""Icon40.png"" ReplaceParameters=""false"">Icon40.png</ProjectItem>
+        <ProjectItem TargetFileName=""Icon58.png"" ReplaceParameters=""false"">Icon58.png</ProjectItem>
+        <ProjectItem TargetFileName=""Icon60.png"" ReplaceParameters=""false"">Icon60.png</ProjectItem>
+        <ProjectItem TargetFileName=""Icon76.png"" ReplaceParameters=""false"">Icon76.png</ProjectItem>
+        <ProjectItem TargetFileName=""Icon80.png"" ReplaceParameters=""false"">Icon80.png</ProjectItem>
+        <ProjectItem TargetFileName=""Icon87.png"" ReplaceParameters=""false"">Icon87.png</ProjectItem>
+        <ProjectItem TargetFileName=""Icon120.png"" ReplaceParameters=""false"">Icon120.png</ProjectItem>
+        <ProjectItem TargetFileName=""Icon152.png"" ReplaceParameters=""false"">Icon152.png</ProjectItem>
+        <ProjectItem TargetFileName=""Icon167.png"" ReplaceParameters=""false"">Icon167.png</ProjectItem>
+        <ProjectItem TargetFileName=""Icon180.png"" ReplaceParameters=""false"">Icon180.png</ProjectItem>
+        <ProjectItem TargetFileName=""Icon1024.png"" ReplaceParameters=""false"">Icon1024.png</ProjectItem>
+      </Folder>
+      <Folder Name=""Properties"" TargetFolderName=""Properties"">";
+            contents = contents.Replace("      <Folder Name=\"Properties\" TargetFolderName=\"Properties\">", assets);
+
             File.WriteAllText(vsTemplate, contents);
         }
 
-        /// <inheritdoc />
         public void CopyItems()
         {
             // Copy Assets.xcassets folder
