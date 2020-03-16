664ef4bee61900b44e629428457d1ac0a893bef7
Anil Kumar <anil.apex@outlook.com>
2020-03-16 23:34:21 +0530

    Removed redundant AssemblyInfo attributes.
    Related Work Items: #584

--- MvvmCrossTest/MvvmCrossTest.Android/Properties/AssemblyInfo.cs
+++ MvvmCrossTest/MvvmCrossTest.Android/Properties/AssemblyInfo.cs
@@ -1,14 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using Android.App;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MvvmCrossTest.Android")]
-[assembly: AssemblyDescription("MvvmCrossTest Android project.")]
-[assembly: AssemblyProduct("MvvmCrossTest.Android")]
 
 
 // Add some common permissions, these can be removed if not needed
