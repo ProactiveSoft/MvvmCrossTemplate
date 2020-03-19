f53c79fb5e0c8aafa89dcffc25c2dc9540c87559
Anil Kumar <anil.apex@outlook.com>
2020-03-19 07:17:30 +0530

    Test: Proper path to SharedAssemblyInfo.cs in csproj.
    Related Work Items: #584

--- MvvmCross.Template.Test/FixCoreShould.cs
+++ MvvmCross.Template.Test/FixCoreShould.cs
@@ -13,15 +13,17 @@ namespace MvvmCross.Template.Test
         [Theory]
         [Trait("File", ".csproj")]
         [CsProjFiles]
-        public void FixCsProj(string csprojFile)
+        public void FixCsProj(string csproj)
         {
             // Arrange
-            _output.WriteLine($"Checking .csproj file for MvvmCrossTest: {csprojFile}");
+            _output.WriteLine($"Checking {csproj}: for MvvmCrossTest &");
+            _output.WriteLine($"\t..\\..\\SharedAssemblyInfo.cs");
 
-            string contents = File.ReadAllText(csprojFile);
+            string contents = File.ReadAllText(csproj);
 
             // Assert
             Assert.DoesNotContain("MvvmCrossTest", contents);
+            Assert.Contains("\"..\\SharedAssemblyInfo.cs\"", contents);
 
             _output.WriteLine("Checked file.");
         }
