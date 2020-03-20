877b27d6e6acc083dcd19db8578684c3fb868e82
Anil Kumar <anil.apex@outlook.com>
2020-03-20 20:39:51 +0530

    Housekeeping
    Related Work Items: #584

--- Temp/Proso.MvvmCross.UWP/Properties/AssemblyInfo.cs
+++ Temp/Proso.MvvmCross.UWP/Properties/AssemblyInfo.cs
@@ -1,10 +1,6 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("$safeprojectname$")]
-[assembly: AssemblyDescription("$ext_safeprojectname$ UWP project.")]
-[assembly: AssemblyProduct("$safeprojectname$")]
