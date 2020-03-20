341445aaf21ddd914adbb5819d3e8f51beb2800e
Anil Kumar <anil.apex@outlook.com>
2020-03-21 02:02:22 +0530

    Added xUnit android project.
    Related Work Items: #863

---
+++ MvvmCrossTest/MvvmCrossTest.Test.Core/MainViewModelShould.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace MvvmCrossTest.Test.Core
+{
+    public class MainViewModelShould
+    {
+        [Fact]
+        public void SetTitle()
+        {
+            // Arrange
+            string expected = "V 6.4.2";
+
+            // Act
+            Assert.Equal(expected, "V 6.4.2");
+        }
+    }
+}
