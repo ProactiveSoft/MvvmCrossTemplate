d1df4d97498c0db89cb281ca2324aa56b8aa8977
Anil Kumar <anil.apex@outlook.com>
2020-04-09 17:41:16 +0530

    Fix assembly version.
    Related Work Items: #868

--- MvvmCross.Template/IFixMetadata.cs
+++ MvvmCross.Template/IFixMetadata.cs
@@ -24,13 +24,16 @@ namespace MvvmCross.Template
             }
         }
 
-
         protected internal (int Year, int Month, int Day, int Seconds) CurrentAppVersion
         {
             get
             {
                 DateTime today = DateTime.Today, now = DateTime.Now;
                 int totalSeconds = (int)(now - today).TotalSeconds;
+                // Normalize multiplier = Max uint / no.of sec in 1 day
+                // Source: Normalize no. between 0-1: https://stackoverflow.com/a/39776893/4038979
+                double normalizeMultiplier = (double)65_534 / 86_400;
+                totalSeconds = (int)Math.Round(totalSeconds * normalizeMultiplier);
                 return (today.Year, today.Month, today.Day, totalSeconds);
             }
         }
@@ -42,6 +45,11 @@ namespace MvvmCross.Template
                 var version = CurrentAppVersion;
                 DateTime month = new DateTime(version.Year, version.Month, 1);
                 int totalSeconds = (int)(DateTime.Now - month).TotalSeconds;
+                // Normalize multiplier = Max uint / no.of sec in 1 month
+                // Source: Normalize no. between 0-1: https://stackoverflow.com/a/39776893/4038979
+                double normalizeMultiplier = (double)65_534 / 26_78_400;
+                totalSeconds = (int)Math.Round(totalSeconds * normalizeMultiplier);
+
                 return (version.Year, version.Month, totalSeconds);
             }
         }
