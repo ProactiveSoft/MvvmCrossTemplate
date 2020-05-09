137c592d47f2d2d984172e994019485070d1ddf3
Anil Kumar <anil.apex@outlook.com>
2020-05-10 01:17:31 +0530

    Doc: Test manifest.
    Related Work Items: #930

---
+++ MvvmCross.Template.Test/FixPlatformSpecificIssues/FixManifestShould.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace MvvmCross.Template.Test.FixPlatformSpecificIssues
+{
+    public class FixManifestShould
+    {
+        public FixManifestShould(ITestOutputHelper output) => _output = output;
+
+
+
+        #region Test MvvmCrossTest Not in Manifest
+        [Trait("Task", "Fix Manifest")]
+        [Theory]
+        [InlineData(@"D:\Plugins\MvvmCrossTest\Temp\Proso.MvvmCross.Android\Properties\AndroidManifest.xml")]
+        [InlineData(@"D:\Plugins\MvvmCrossTest\Temp\Proso.MvvmCross.Test.Droid\Properties\AndroidManifest.xml")]
+        [InlineData(@"D:\Plugins\MvvmCrossTest\Temp\Proso.MvvmCross.UWP\Package.appxmanifest")]
+        [InlineData(@"D:\Plugins\MvvmCrossTest\Temp\Proso.MvvmCross.Test.UWP\Package.appxmanifest")]
+        [InlineData(@"D:\Plugins\MvvmCrossTest\Temp\Proso.MvvmCross.iOS\Info.plist")]
+        [InlineData(@"D:\Plugins\MvvmCrossTest\Temp\Proso.MvvmCross.Test.iOS\Info.plist")]
+        public void CheckMvvmCrossTestNotInManifest(string manifest)
+        {
+            // Arrange
+            string contents = File.ReadAllText(manifest),
+                expectedValue = "MvvmCrossTest";
+            _output.WriteLine($"{manifest}: Checking {expectedValue} is not present ...");
+
+            // Assert
+            Assert.DoesNotContain(expectedValue, contents);
+
+            _output.WriteLine("Checked.");
+        }
+        #endregion
+
+
+
+        private readonly ITestOutputHelper _output;
+    }
+}
