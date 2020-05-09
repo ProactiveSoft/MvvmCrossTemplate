a39af611a2171cd963b38d70b084ad7589a64411
Anil Kumar <anil.apex@outlook.com>
2020-05-10 01:55:40 +0530

    Removed redundant code.
    Related Work Items: #930

--- MvvmCross.Template.Test/FixIosProjectShould.cs
+++ MvvmCross.Template.Test/FixIosProjectShould.cs
@@ -11,6 +11,7 @@ namespace MvvmCross.Template.Test
         public FixIosProjectShould(ITestOutputHelper output) => _output = output;
 
 
+
         [Fact]
         [Trait("Task", "Fix root")]
         public void CopyAssetsFolder()
@@ -50,37 +51,6 @@ namespace MvvmCross.Template.Test
                 $"Assets folder successfully copied.{Environment.NewLine}Random file {randomSourceFileName} is present in destination folder");
         }
 
-        [Fact]
-        [Trait("File", ".vstemplate")]
-        public void IncludeAssetsInVsTemplate()
-        {
-            // Arrange
-            string vsTemplate =
-                File.ReadAllText(@"D:\Plugins\MvvmCrossTest\Temp\Proso.MvvmCross.iOS\MyTemplate.vstemplate");
-            string assets =
-                @"      <Folder Name=""Assets.xcassets"" TargetFolderName=""Assets.xcassets"">
-        <Folder Name=""AppIcon.appiconset"" TargetFolderName=""AppIcon.appiconset"">
-          <ProjectItem TargetFileName=""Contents.json"" ReplaceParameters=""true"">Contents.json</ProjectItem>
-          <ProjectItem TargetFileName=""Icon20.png"" ReplaceParameters=""false"">Icon20.png</ProjectItem>
-          <ProjectItem TargetFileName=""Icon29.png"" ReplaceParameters=""false"">Icon29.png</ProjectItem>
-          <ProjectItem TargetFileName=""Icon40.png"" ReplaceParameters=""false"">Icon40.png</ProjectItem>
-          <ProjectItem TargetFileName=""Icon58.png"" ReplaceParameters=""false"">Icon58.png</ProjectItem>
-          <ProjectItem TargetFileName=""Icon60.png"" ReplaceParameters=""false"">Icon60.png</ProjectItem>
-          <ProjectItem TargetFileName=""Icon76.png"" ReplaceParameters=""false"">Icon76.png</ProjectItem>
-          <ProjectItem TargetFileName=""Icon80.png"" ReplaceParameters=""false"">Icon80.png</ProjectItem>
-          <ProjectItem TargetFileName=""Icon87.png"" ReplaceParameters=""false"">Icon87.png</ProjectItem>
-          <ProjectItem TargetFileName=""Icon120.png"" ReplaceParameters=""false"">Icon120.png</ProjectItem>
-          <ProjectItem TargetFileName=""Icon152.png"" ReplaceParameters=""false"">Icon152.png</ProjectItem>
-          <ProjectItem TargetFileName=""Icon167.png"" ReplaceParameters=""false"">Icon167.png</ProjectItem>
-          <ProjectItem TargetFileName=""Icon180.png"" ReplaceParameters=""false"">Icon180.png</ProjectItem>
-          <ProjectItem TargetFileName=""Icon1024.png"" ReplaceParameters=""false"">Icon1024.png</ProjectItem>
-        </Folder>
-      </Folder>";
-
-            // Assert
-            Assert.Contains(assets, vsTemplate);
-            _output.WriteLine("Assets included in iOS's .vstemplate.");
-        }
 
 
         private readonly ITestOutputHelper _output;
