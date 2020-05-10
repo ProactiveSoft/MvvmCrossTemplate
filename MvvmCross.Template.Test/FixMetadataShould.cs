22c3c702bb049d19fffa5eca52d832d57aa7571b
Anil Kumar <anil.apex@outlook.com>
2020-05-10 21:47:10 +0530

    Doc: Test updating Mvx & template version.
    Related Work Items: #930

--- MvvmCross.Template.Test/FixMetadataShould.cs
+++ MvvmCross.Template.Test/FixMetadataShould.cs
@@ -16,60 +16,6 @@ namespace MvvmCross.Template.Test
 
         [Fact]
         [Trait("Task", "Fix root")]
-        public void UpdateMvxVersion()
-        {
-            // Arrange
-            // ToDo: Get latest MvvmCross version "expectedVersion" by scraping
-            // https://github.com/MvvmCross/MvvmCross/blob/develop/CHANGELOG.md
-            string expectedVersion = "6.4.2",
-                mainVm = Path.Combine(TemplateFolder, "Proso.MvvmCross.Core", "ViewModels", "MainViewModel.cs"),
-                vsTemplate = Path.Combine(TemplateFolder, "Proso-MvvmCross-Xamarin-Template.vstemplate"),
-                mainVmVersionLine = string.Empty, vsTemplateVersionLine = string.Empty;
-
-            foreach (var line in File.ReadAllLines(mainVm))
-                if (line.StartsWith("            Title ="))
-                {
-                    mainVmVersionLine = line;
-                    break;
-                }
-
-            foreach (var line in File.ReadAllLines(vsTemplate))
-                if (line.StartsWith("    <Description>"))
-                {
-                    vsTemplateVersionLine = line;
-                    break;
-                }
-
-            // Assert
-            Assert.Contains(expectedVersion, mainVmVersionLine);
-            Assert.Contains(expectedVersion, vsTemplateVersionLine);
-
-            _output.WriteLine($"MvvmCross template updated to version {expectedVersion}.");
-        }
-
-        [Fact]
-        [Trait("Task", "Fix root")]
-        public void UpdateTemplateVersion()
-        {
-            // Arrange
-            string directoryBuildProps = Path.Combine(TemplateFolder, "Directory.Build.props");
-            string contents = File.ReadAllText(directoryBuildProps);
-            IFixMetadata fixMetadata = new FixTemplateRoot(new FolderHelper());
-            var (year, month, day, _) = fixMetadata.CurrentAppVersion;
-            string informationalVersion = $"<InformationalVersion>{year}.{month}",
-                fileVersion = $"<FileVersion>{year}.{month}.{day}",
-                copyright = $"<Copyright>© Proso {year}";
-
-            // Act
-            Assert.Contains(informationalVersion, contents);
-            Assert.Contains(fileVersion, contents);
-            Assert.Contains(copyright, contents);
-
-            _output.WriteLine($"Template version is latest: {year}.{month}");
-        }
-
-        [Fact]
-        [Trait("Task", "Fix root")]
         public void FixSolutionMetadata()
         {
             // Arrange
---
+++ MvvmCross.Template.Test/FixRootIssues/UpdateVersionShould.cs
@@ -0,0 +1,77 @@
+﻿using System.IO;
+using MvvmCross.Template.Helpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace MvvmCross.Template.Test.FixRootIssues
+{
+    public class UpdateVersionShould
+    {
+        public UpdateVersionShould(ITestOutputHelper output) => _output = output;
+
+
+
+        #region Test Update Mvx Version
+        [Fact]
+        [Trait("Task", "Fix root")]
+        public void UpdateMvxVersion()
+        {
+            // Arrange
+            // ToDo: Get latest MvvmCross version "expectedVersion" by scraping
+            // https://github.com/MvvmCross/MvvmCross/blob/develop/CHANGELOG.md
+            string expectedVersion = "6.4.2",
+                mainVm = Path.Combine(TemplateFolder, "Proso.MvvmCross.Core", "ViewModels", "MainViewModel.cs"),
+                vsTemplate = Path.Combine(TemplateFolder, "Proso-MvvmCross-Xamarin-Template.vstemplate"),
+                mainVmVersionLine = string.Empty, vsTemplateVersionLine = string.Empty;
+
+            foreach (var line in File.ReadAllLines(mainVm))
+                if (line.StartsWith("            Title ="))
+                {
+                    mainVmVersionLine = line;
+                    break;
+                }
+
+            foreach (var line in File.ReadAllLines(vsTemplate))
+                if (line.StartsWith("    <Description>"))
+                {
+                    vsTemplateVersionLine = line;
+                    break;
+                }
+
+            // Assert
+            Assert.Contains(expectedVersion, mainVmVersionLine);
+            Assert.Contains(expectedVersion, vsTemplateVersionLine);
+
+            _output.WriteLine($"MvvmCross template updated to version {expectedVersion}.");
+        }
+        #endregion
+
+        #region Test Update Template Version
+        [Fact]
+        [Trait("Task", "Fix root")]
+        public void UpdateTemplateVersion()
+        {
+            // Arrange
+            string directoryBuildProps = Path.Combine(TemplateFolder, "Directory.Build.props");
+            string contents = File.ReadAllText(directoryBuildProps);
+            IFixMetadata fixMetadata = new FixTemplateRoot(new FolderHelper());
+            var (year, month, day, _) = fixMetadata.CurrentAppVersion;
+            string informationalVersion = $"<InformationalVersion>{year}.{month}",
+                fileVersion = $"<FileVersion>{year}.{month}.{day}",
+                copyright = $"<Copyright>© Proso {year}";
+
+            // Act
+            Assert.Contains(informationalVersion, contents);
+            Assert.Contains(fileVersion, contents);
+            Assert.Contains(copyright, contents);
+
+            _output.WriteLine($"Template version is latest: {year}.{month}");
+        }
+        #endregion
+
+
+
+        private const string TemplateFolder = @"D:\Plugins\MvvmCrossTest\Temp";
+        private readonly ITestOutputHelper _output;
+    }
+}
